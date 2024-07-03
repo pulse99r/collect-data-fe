ce717a19f11c5c0b49780daf04f117692d8c3641
Raymond <raymondjohnson@pursuit.org>
2024-07-03 12:19:18 -0400

    init: initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className='nav'>
+          <ul className='menu'>
+            <li className='menu-item'>Data Entry</li>
+            <li className='menu-item'>View Users</li>
+          </ul>
+        </nav>
       </header>
+      <main>
+        <form action="submit" id='form' className='form' >
+          <input id='fname' type="text" placeholder="enter first name"/>
+          <input id='lname' type="text" placeholder="enter last name"/>
+          <input id='city' type="text" placeholder="enter city"/>
+          <input id='website' type="text" placeholder="enter website"/>
+          <span className="btn-group">
+            <button id='submit' type="submit">Submit</button>
+            <button id='clear' type="clear">Clear</button>
+            <button id='upload' type="upload">Upload</button>
+          </span>
+        </form>
+      </main>
     </div>
   );
 }
